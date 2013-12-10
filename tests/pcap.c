aa48c75644e176de5c6d1e7387212d4663b6524c
Martin Peres <martin.peres@labri.fr>
2013-12-10 15:37:36 +0100

    test/pcap: add support for reading pcap files

--- tests/pcap.c
+++ tests/pcap.c
@@ -1,12 +1,15 @@
-#include <pcap.h>
 #include <stdio.h>
 #include <assert.h>
 #include <rtgde.h>
+#include <stdlib.h>
 
 #include <predictions/simple.h>
 #include <models/dummy.h>
 
 #define NETIF_DATARATE 6000000 // 48 MBit/s
+
+#ifdef HAS_LIBPCAP
+#include <pcap.h>
 
 int64_t relative_time_us()
 {
@@ -60,6 +63,100 @@ int capture_packets(metric_t * me, int64_t timeout_us)
 	pcap_close(handle);
 	return(0);
 }
+#endif
+
+struct pcap_packet {
+	uint64_t timestamp;
+	uint32_t len;
+};
+
+void die(const char *str)
+{
+	perror(str);
+	exit(1);
+}
+
+int pcap_skip_header(FILE *file)
+{
+	uint32_t magic;
+
+	fread(&magic, sizeof(uint32_t), 1, file);
+	if (magic != 0xa1b2c3d4)
+		return 1;
+
+	fseek(file, 24, SEEK_SET);
+	return 0;
+}
+
+int pcap_read_packet_header(FILE *file, struct pcap_packet *pkt)
+{
+	uint32_t ts_sec, ts_usec, incl_len, orig_len;
+	int ret;
+
+	ret = fread(&ts_sec, sizeof(uint32_t), 1, file);
+	ret += fread(&ts_usec, sizeof(uint32_t), 1, file);
+	ret += fread(&incl_len, sizeof(uint32_t), 1, file);
+	ret += fread(&orig_len, sizeof(uint32_t), 1, file);
+	if (ret != 4)
+		return 1;
+
+	fseek(file, incl_len, SEEK_CUR);
+
+	pkt->timestamp = ts_sec * 1000000 + ts_usec;
+	pkt->len = orig_len;
+
+	return 0;
+}
+
+void read_from_file(metric_t * me, const char *filepath)
+{
+	struct pcap_packet pkt;
+
+	FILE *finput = fopen(filepath, "rb");
+	if (!finput)
+		die("Cannot open the input file");
+
+	if (pcap_skip_header(finput)) {
+		fprintf(stderr, "Invalid magic number. The file '%s' isn't a pcap file\n", filepath);
+	}
+
+	while (!pcap_read_packet_header(finput, &pkt))
+	{
+		uint64_t time_end = pkt.timestamp + pkt.len * 1000000 / NETIF_DATARATE;
+
+		metric_update(me, pkt.timestamp - 1, 0);
+		metric_update(me, pkt.timestamp, NETIF_DATARATE);
+		metric_update(me, time_end, NETIF_DATARATE);
+		metric_update(me, time_end + 1, 0);
+	}
+
+	/* wait for the prediction to be done and quit */
+	sleep(1);
+}
+
+void usage(int argc, char **argv)
+{
+#ifdef HAS_LIBPCAP
+	fprintf(stderr, "Usage: %s [filepath]\n", argv[0]);
+#else
+	fprintf(stderr, "Usage: %s filepath\n", argv[0]);
+#endif
+}
+
+void do_work(int argc, char *argv[], metric_t * me, int64_t timeout_us)
+{
+#ifdef HAS_LIBPCAP
+	if (argc == 1) {
+		capture_packets(me, timeout_us);
+		return;
+	}
+#endif
+
+	if (argc == 2)
+		read_from_file(me, argv[1]);
+	else
+		usage(argc, argv);
+}
 
 int main(int argc, char *argv[])
 {
@@ -82,7 +179,7 @@ int main(int argc, char *argv[])
 
 	rtgde_start(f);
 
-	capture_packets(me, 10000000);
+	do_work(argc, argv, me, 10000000);
 
 	flowgraph_teardown(f);
 
