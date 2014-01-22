88b8b73e1b411d5ad26b83356392399d684a3595
Martin Peres <martin.peres@labri.fr>
2014-01-22 20:03:47 +0100

    scoring: forgot two files

---
+++ include/scoring/simple.h
@@ -0,0 +1,16 @@
+#ifndef SCORE_SIMPLE_H
+#define SCORE_SIMPLE_H
+
+#include "../score.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+scoring_t * score_simple_create();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ lib/scoring/simple.c
@@ -0,0 +1,28 @@
+#include "score.h"
+#include "../score_priv.h"
+
+typedef struct {
+
+} score_simple_t;
+
+int score_simple_calc(scoring_t *s, const prediction_t *p, const graph_t *model_out)
+{
+	return 0;
+}
+
+void score_simple_dtor(scoring_t *s)
+{
+	free(s->user);
+}
+
+scoring_t * score_simple_create()
+{
+	score_simple_t *simple = malloc(sizeof(score_simple_t));
+	if (!simple)
+		return NULL;
+
+	return score_create(score_simple_calc, score_simple_dtor,
+			    "score_simple", (void *)simple);
+}
+
+
