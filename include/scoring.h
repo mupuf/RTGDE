6563baff479542db4649427bbba0125a9cde9b9d
Martin Peres <martin.peres@labri.fr>
2014-02-10 11:01:00 +0100

    scoring: scoring systems should be creatable out of the tree

--- include/scoring.h
+++ include/scoring.h
@@ -21,6 +21,13 @@ scoring_metric_t * scoring_metric_by_name(scoring_t *s, const char *name);
 int scoring_metric_weight(scoring_metric_t *metric);
 void scoring_metric_delete(scoring_metric_t *metric);
 
+typedef float (*scoring_calc_t)(scoring_t *s, const prediction_metric_result_t *pmr,
+				const graph_t *model_out);
+typedef void (*scoring_dtor_t)(scoring_t *s);
+
+scoring_t *scoring_create(scoring_calc_t calc, scoring_dtor_t dtor,
+			  const char *name, void *user);
+void *scoring_user(scoring_t *s);
 const char *scoring_name(scoring_t *s);
 int scoring_exec(scoring_t *s, decision_input_t *di);
 
--- lib/scoring_priv.h
+++ lib/scoring_priv.h
@@ -4,9 +4,6 @@
 #include "scoring.h"
 #include "prediction.h"
 
-typedef float (*scoring_calc_t)(scoring_t *s, const prediction_metric_result_t *pmr, const graph_t *model_out);
-typedef void (*scoring_dtor_t)(scoring_t *s);
-
 typedef struct {
 	scoring_t base;
 
@@ -27,9 +24,5 @@ typedef struct {
 	char *name;
 	int weight;
 } scoring_metric_priv_t;
-
-scoring_t *scoring_create(scoring_calc_t calc, scoring_dtor_t dtor,
-			  const char *name, void *user);
-void *scoring_user(scoring_t *s);
 
 #endif // SCORE_PRIV_H
